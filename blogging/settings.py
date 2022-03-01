2c5d9576300b78ac98b5b9e73785e132dbe043a5
atmadevrt99 <atmadevrt99@gmail.com>
2022-03-01 08:23:22 +0530

    COM

--- blogging/settings.py
+++ blogging/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!jjij&!t!lw@r=ne6!##_yh9cwz-tu6q63rl7ohiut_jq8e69-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_DIR=BASE_DIR/'static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR]
+#STATICFILES_DIRS=[STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
